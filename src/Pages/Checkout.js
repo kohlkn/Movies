06490937a96083f2f35339610ca472b7eac81ba6
samirhadii <samirhadi929@gmail.com>
2022-12-08 02:12:29 -0500

    review using orders collection

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -114,8 +114,7 @@ export default function Checkout() {
                 </Typography>
                 <Typography variant="subtitle1">
                   Your order number is #2001539. We have emailed your order
-                  confirmation, and will send you an update when your order has
-                  shipped.
+                  confirmation.
                 </Typography>
               </React.Fragment>
             ) : (
--- src/Pages/Review.js
+++ src/Pages/Review.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -8,59 +9,65 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import firebase from 'firebase/compat/app';
 import { db } from '../firebase';
+import { collection, onSnapshot} from 'firebase/firestore';
+import firebase from 'firebase/compat/app';
 import "firebase/auth";
-import { getDatabase, ref, child, get } from "firebase/database";
-import { useState, useEffect } from 'react'
-import { collection, deleteDoc, onSnapshot, doc } from 'firebase/firestore';
 
-const products = [
-  {
-    name: 'The Lego Movie',
-    desc: '0-8yrs',
-    price: '$8.00',
-  },
-  {
-    name: 'The Lego Movie',
-    desc: '9-17yrs',
-    price: '$9.00',
-  },
-  {
-    name: 'The Lego Movie',
-    desc: '18-64yrs',
-    price: '$10.00',
-  },
-  {
-    name: 'The Lego Movie',
-    desc: '+65yrs',
-    price: '$6.00',
-  },
-  //{ name: 'Shipping', desc: '', price: 'Free' },
-];
+// const products = [
+//   {
+//     name: 'The Lego Movie',
+//     desc: '0-8yrs',
+//     price: '$8.00',
+//   },
+//   {
+//     name: 'The Lego Movie',
+//     desc: '9-17yrs',
+//     price: '$9.00',
+//   },
+//   {
+//     name: 'The Lego Movie',
+//     desc: '18-64yrs',
+//     price: '$10.00',
+//   },
+//   {
+//     name: 'The Lego Movie',
+//     desc: '+65yrs',
+//     price: '$6.00',
+//   },
+//   //{ name: 'Shipping', desc: '', price: 'Free' },
+// ];
 
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
-const payments = [
-  { name: 'Card type', detail: 'Visa' },
-  { name: 'Card holder', detail: 'Mr John Smith' },
-  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-  { name: 'Expiry date', detail: '04/2024' },
-];
+// const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
+// const payments = [
+//   { name: 'Card type', detail: 'Visa' },
+//   { name: 'Card holder', detail: 'Mr John Smith' },
+//   { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
+//   { name: 'Expiration', detail: '04/2024' },
+// ];
+
+//edit this value later to pull from database and be altered by promo codes 
+let total = 10.00
 
 export default function Review() {
-
-  const [order, setOrder] = useState([]);
+  //gets booking from current user
+  let user = firebase.auth().currentUser;
+  const userid = user.uid;
+  const [users, setUsers] = useState([]);
   const [loading,setLoading] = useState(false);
+  const [infoloading,setInfoLoading] = useState(false);
+  const [usersInfo, setUsersInfo] = useState([]);
 
   useEffect(() => {
     setLoading(true);
-    //the collection db, "movies" calls upon a database named movies in the firebase setup
     const unsub = onSnapshot(collection(db,"order"), (snapshot) => {
       let list = [];
       snapshot.docs.forEach((doc) => {
+        if(doc.id == "new"){
         list.push({id: doc.id, ...doc.data()})
+        }
       });
-      setOrder(list);
+      setUsers(list);
       //setSearchResults(list);
       setLoading(false)
   
@@ -71,19 +78,41 @@ export default function Review() {
       unsub();
     };
   }, []);
-
+  //gets current user billing and card information
+  useEffect(() => {
+    setInfoLoading(true);
+    const unsub = onSnapshot(collection(db,"users"), (snapshot) => {
+      let list = [];
+      snapshot.docs.forEach((doc) => {
+        if(doc.id == userid){
+        list.push({id: doc.id, ...doc.data()})
+        }
+      });
+      setUsersInfo(list);
+      //setSearchResults(list);
+      setInfoLoading(false)
+  
+    }, (error)=> {
+      console.log(error);
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+  
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Order summary
       </Typography>
       <List disablePadding>
-        {order && order.map((item) => (
+      {users && users.map((item) => (
           <ListItem key={item.movieId} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={item.movieId} secondary={'Seats: ' + item.seats} />
-            <Typography variant="body2">${item.ticketcost * item.ticketnum}</Typography>
-            <Button style= {{ color: 'red'}}><AddIcon /></Button>
-            <Button style= {{ color: 'red'}}><DeleteIcon /></Button>
+            <ListItemText primary={item.movieId} secondary={item.seats} />
+            <Typography variant="body3">Price: ${item.ticketcost}</Typography>
+            <Typography variant="body2">x{item.ticketnum}</Typography>
+            {/* <Button style= {{ color: 'red'}}><AddIcon /></Button>
+            <Button style= {{ color: 'red'}}><DeleteIcon /></Button> */}
           </ListItem>
         ))}
         <TextField
@@ -93,44 +122,44 @@ export default function Review() {
             color='error'
           />
         <ListItem sx={{ py: 1, px: 0 }}>
-          <ListItemText primary="Shipping" />
-          <Typography variant="subtitle1" sx={{ fontWeight: 400 }}>
-            Free
-          </Typography>
-        </ListItem>
-
-        <ListItem sx={{ py: 1, px: 0 }}>
+          
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $33.00
+            ${total}
           </Typography>
         </ListItem>
       </List>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Shipping
-          </Typography>
-          <Typography gutterBottom>John Smith</Typography>
-          <Typography gutterBottom>{addresses.join(', ')}</Typography>
-        </Grid>
-        <Grid item container direction="column" xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Payment details
-          </Typography>
-          <Grid container>
-            {payments.map((payment) => (
-              <React.Fragment key={payment.name}>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.name}</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.detail}</Typography>
-                </Grid>
-              </React.Fragment>
-            ))}
-          </Grid>
-        </Grid>
+        {usersInfo && usersInfo.map((item) => (
+        <><Grid item xs={12} sm={6}>
+            <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+              Billing
+            </Typography>
+            <Typography gutterBottom>{item.cardname}</Typography>
+            <Typography gutterBottom>{item.addone}</Typography>
+            <Typography gutterBottom>{item.addtwo}</Typography>
+            <Typography gutterBottom>{item.city}</Typography>
+            <Typography gutterBottom>{item.state}</Typography>
+          </Grid><Grid item container direction="column" xs={12} sm={6}>
+              <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+                Payment details
+              </Typography>
+              <Grid container>
+                  <React.Fragment key={item.cardname}>
+                    <Grid item xs={6}>
+                      <Typography gutterBottom>{item.cardname}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography gutterBottom>{item.cardnum}</Typography>
+                      <Typography gutterBottom>{item.exp}</Typography>
+                      <Typography gutterBottom>{item.cvv}</Typography>
+                      
+                    </Grid>
+                  </React.Fragment>
+              </Grid>
+            </Grid>
+            </>
+        ))}
       </Grid>
     </React.Fragment>
   );
