45647be96f7dd58a84e819b87bad076b5d48622f
samirhadii <samirhadi929@gmail.com>
2022-12-07 20:35:39 -0500

    Update Navbar.js
    updated navbar to only display edit profile and profile when logged in

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,46 +5,133 @@ import "firebase/auth";
 
 
   
-const Navbar = () => {
+// const Navbar = () => {
+  function Navbar(){
+
 let user = firebase.auth().currentUser;
-  return (
-    <>
-      <Nav>
-        <NavMenu>
-          <NavLink to="/MainPage" activestyle='true'>
-            Home
-          </NavLink>
-          <NavLink to="/SignUp" activestyle='true'>
-            Sign Up
-          </NavLink>
-          <NavLink to="/SignIn" activestyle='true'>
-            Login
-          </NavLink>
-          <NavLink to="/Checkout" activestyle='true'>
-            Checkout
-          </NavLink>
-          <NavLink to="/RegistrationConfirm" activestyle='true'>
-            Registration Confirmation
-          </NavLink>
-          <NavLink to="/BookTickets" activestyle='true'>
-            Book
-          </NavLink>
-          <NavLink to="/AdminView" activestyle='true'>
-            Admin
+
+//function isUserLoggedIn(){
+  if (user){
+    return (
+      <>
+        <Nav>
+          <NavMenu>
+            <NavLink to="/MainPage" activestyle='true'>
+              Home
             </NavLink>
-          <NavLink to="/Profile" activestyle='true'>
-            Profile
-          </NavLink>
-          <NavLink to="/EditPage" activestyle='true'>
-            Edit profile
-          </NavLink>
-          {/* <NavLink to='/Logout' activestyle='true'>
-            Logout
-          </NavLink> */}
-        </NavMenu>
-      </Nav>
-    </>
-  );
+            <NavLink to="/SignUp" activestyle='true'>
+              Sign Up
+            </NavLink>
+            <NavLink to="/SignIn" activestyle='true'>
+              Login
+            </NavLink>
+            <NavLink to="/Checkout" activestyle='true'>
+              Checkout
+            </NavLink>
+            <NavLink to="/RegistrationConfirm" activestyle='true'>
+              Registration Confirmation
+            </NavLink>
+            <NavLink to="/BookTickets" activestyle='true'>
+              Book
+            </NavLink>
+            {/* <NavLink to="/AdminView" activestyle='true'>
+              Admin
+              </NavLink> */}
+            <NavLink to="/Profile" activestyle='true'>
+              Profile
+            </NavLink>
+            <NavLink to="/EditPage" activestyle='true'>
+              Edit profile
+            </NavLink>
+            {/* <NavLink to='/Logout' activestyle='true'>
+              Logout
+            </NavLink> */}
+          </NavMenu>
+        </Nav>
+      </>
+    );
+
+  }
+  else{
+    return (
+      <>
+        <Nav>
+          <NavMenu>
+            <NavLink to="/MainPage" activestyle='true'>
+              Home
+            </NavLink>
+            <NavLink to="/SignUp" activestyle='true'>
+              Sign Up
+            </NavLink>
+            <NavLink to="/SignIn" activestyle='true'>
+              Login
+            </NavLink>
+            <NavLink to="/Checkout" activestyle='true'>
+              Checkout
+            </NavLink>
+            <NavLink to="/RegistrationConfirm" activestyle='true'>
+              Registration Confirmation
+            </NavLink>
+            <NavLink to="/BookTickets" activestyle='true'>
+              Book
+            </NavLink>
+            {/* <NavLink to="/AdminView" activestyle='true'>
+              Admin
+              </NavLink> */}
+            {/* <NavLink to="/Profile" activestyle='true'>
+              Profile
+            </NavLink> */}
+            {/* <NavLink to="/EditPage" activestyle='true'>
+              Edit profile
+            </NavLink> */}
+            {/* <NavLink to='/Logout' activestyle='true'>
+              Logout
+            </NavLink> */}
+          </NavMenu>
+        </Nav>
+      </>
+    );
+    
+  }
+
+  // return (
+  //   <>
+  //     <Nav>
+  //       <NavMenu>
+  //         <NavLink to="/MainPage" activestyle='true'>
+  //           Home
+  //         </NavLink>
+  //         <NavLink to="/SignUp" activestyle='true'>
+  //           Sign Up
+  //         </NavLink>
+  //         <NavLink to="/SignIn" activestyle='true'>
+  //           Login
+  //         </NavLink>
+  //         <NavLink to="/Checkout" activestyle='true'>
+  //           Checkout
+  //         </NavLink>
+  //         <NavLink to="/RegistrationConfirm" activestyle='true'>
+  //           Registration Confirmation
+  //         </NavLink>
+  //         <NavLink to="/BookTickets" activestyle='true'>
+  //           Book
+  //         </NavLink>
+  //         <NavLink to="/AdminView" activestyle='true'>
+  //           Admin
+  //           </NavLink>
+  //         <NavLink to="/Profile" activestyle='true'>
+  //           Profile
+  //         </NavLink>
+  //         <NavLink to="/EditPage" activestyle='true'>
+  //           Edit profile
+  //         </NavLink>
+  //         {/* <NavLink to='/Logout' activestyle='true'>
+  //           Logout
+  //         </NavLink> */}
+  //       </NavMenu>
+  //     </Nav>
+  //   </>
+  // );
 };
   
-export default Navbar;
+export default  Navbar;
